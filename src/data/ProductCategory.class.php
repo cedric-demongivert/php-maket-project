23d50090833a2e6056894674f33ecb51e77d4939
Manegan <iskandaroff.yenrick@gmail.com>
2014-01-10 12:18:04 +0100

    Ajout data class

--- src/data/ProductCategory.class.php
+++ src/data/ProductCategory.class.php
@@ -1,1 +1,18 @@
 <?php
+
+class ProductCategory {
+	private $id;
+	private $name;
+	
+	function __construct($name) {
+		$this->name = $name;
+	}
+	
+	function __construct($bdd,$id) {
+		$result = $bdd->prepare("SELECT * FROM categorie WHERE num_categorie = :id");
+		$result->bindParam(":id",$id);
+		$result->execute() or die("Erreur SQL ligne 14 ProductCategory");
+	}
+}
+
+?>

0edcd4e7e2002d1fbb6d6c74b66e26d3cfa2c328
Manegan <iskandaroff.yenrick@gmail.com>
2014-01-13 09:41:01 +0100

    COntroller Category

--- controllers/Categories.class.php
+++ controllers/Categories.class.php
@@ -4,7 +4,7 @@ require_once "./src/data/Category.class.php";
 class Categories extends Controller {
 	
 	public function __construct() {
-		parent::__construct("categoriesController", "Categories.template.html");
+		parent::__construct("nav", "Categories.template.html");
 		$this->title = "Navigation";
 	}
 	
@@ -12,9 +12,36 @@ class Categories extends Controller {
 		
 	}
 	
+	public function getCategory() {
+		$category = new Category();
+		if (isset($_GET['id_category'])) {
+			return $category->selectById($_GET['id_category']);
+		}
+	}
+	
 	public function getCategories() {
 		$category = new Category();
-		return Model::toData($category->selectAll());
+		if (isset($_GET['id_category'])) {
+			$categories = $category->select("idParent = ".Model::$bdd->quote($_GET['id_category']));
+			
+			return $categories;
+		} else {
+			$categories = $category->select("idParent = -1");
+			
+			return $categories;
+		}
+	}
+	
+	public function getArticles() {
+		$category = new Category();
+		$id = -1;
+		if (isset($_GET['id_category'])) {
+			$id = $_GET['id_category'];
+			$category = $category->selectById($id);
+		} else {
+			$category->setId($id);
+		}
+		return $category->getSpecificArticles();
 	}
 	
 }

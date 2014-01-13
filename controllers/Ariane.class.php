0a7231b625f9f5a31dd90bdac7fd309beae56244
cdemongi <cedric.demongivert@gmail.com>
2014-01-13 08:48:52 +0100

    ariane

---
+++ controllers/Ariane.class.php
@@ -0,0 +1,47 @@
+<?php
+
+class Ariane extends Controller {
+	
+	public function __construct() {
+		parent::__construct("ariane", "Ariane.template.html");
+		$this->title = "Fil d'ariane";
+	}
+	
+	public function getCategoryTree() {
+		
+		if(isset($_GET['id_category'])) {
+			
+			/* Récupération de la catégorie courrante : */
+			$category = new Category();
+			$category = $category->selectById($_GET['id_category']);
+			
+			if(!empty($category)) {
+				/* Tableau contenant les parents : */
+				$tree = array($category);
+				
+				/* Tant qu'on peut récupérer un parent : */
+				while(($category = $category->getParent()) != null) {
+					$tree[] = $category;
+				}
+				
+				/* Tableau retourné */
+				$returnable = array();
+	
+				/* On inverse $tree */
+				for($i = 1; $i <= sizeof($tree); $i++) {
+					$returnable[] = $tree[sizeof($tree)-$i];
+				}
+				
+				return $returnable;
+			}
+			else {
+				return null;
+			}
+		}
+		
+	}
+	
+	
+}
+
+?>
